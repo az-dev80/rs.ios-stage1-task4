5deb00eb3b12c023a52941e2bdc7a86ddb2d2409
Albert Zhloba <albert.zhloba@gmail.com>
2020-04-13 23:54:57 +0300

    func: implement a method

--- RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
+++ RSSchool_T4/RSSchool_T4/2/SquareDecomposer.m
@@ -2,10 +2,29 @@
 
 @implementation SquareDecomposer
 - (NSArray <NSNumber*>*)decomposeNumber:(NSNumber*)number {
-    if (number.integerValue <0) {
-        return nil;
-    }
-    else  return nil;
+   
+        NSNumber* q = [NSNumber numberWithLongLong:[number longLongValue] * [number longLongValue]];
+            return [self decomposeNumber:number w: q];
+        }
+
+        -(NSMutableArray*)decomposeNumber:(NSNumber*)number w:(NSNumber*)y {
+            NSMutableArray *myArray = [[NSMutableArray alloc] init];
+            if ([y longLongValue] == 0) {
+                return myArray;
+            }
+            
+            for (long long i = [number intValue] - 1; i > 0; i--) {
+                long long z = [y longLongValue] - i * i;
+                if (z >= 0) {
+                    myArray = [self decomposeNumber: [NSNumber numberWithLongLong: i]
+                                         w:[NSNumber numberWithLongLong: z]];
+                    if (myArray != nil) {
+                        [myArray addObject: [NSNumber numberWithLongLong: i]];
+                        return myArray;
+                    }
+                }
+            }
+            return nil;
         
         
      //   return @[@0];
